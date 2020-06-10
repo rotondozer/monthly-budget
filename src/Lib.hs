730e922366516df5a6c7dbf4186bb64204b5da61
Nick Rotondo <nickrotondo724@gmail.com>
2020-06-10 18:30:36 -0400

    use Map.fromListWith to add repeats of expenses

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,23 +7,24 @@ import qualified Data.Map                      as Map
 import           Data.Maybe                     ( fromMaybe )
 import qualified Table
 
-type Expenses = Map.Map String Float
+type Expenses = Map.Map String String
 
 monthlyBudget :: String -> Expenses
 monthlyBudget csv = expenses (Table.toTable csv)
 
 expenses :: Table.Table -> Expenses
-expenses table@(_ : rows) = Map.fromList
-    $ map (\r -> (description r, amount r)) rows
+expenses table@(_ : rows) = Map.fromListWith addAmounts
+    $ map (\row -> (descriptionFrom row, amountFrom row)) rows
   where
-    amount      = getAmount table
-    description = getDescription table
+    amountFrom      = getAmount table
+    descriptionFrom = getDescription table
 
+addAmounts :: String -> String -> String
+addAmounts a1 a2 = show $ (read a1 :: Float) + (read a2 :: Float)
 
 getDescription :: Table.Table -> Table.Row -> String
 getDescription table row =
     fromMaybe "No Description" (Table.getCell table "\"Description\"" row)
 
-getAmount :: Table.Table -> Table.Row -> Float
-getAmount table row =
-    read (fromMaybe "0" (Table.getCell table "\"Amount\"" row)) :: Float
+getAmount :: Table.Table -> Table.Row -> String
+getAmount table row = fromMaybe "0" (Table.getCell table "\"Amount\"" row)
