fdc296f6677165d2ca006382a5aaa6a7d0cc5085
Nick Rotondo <nickrotondo724@gmail.com>
2020-07-03 14:46:45 -0400

    Move csv parser stuff out of Table module

---
+++ src/Parser.hs
@@ -0,0 +1,39 @@
+module Parser
+    ( parseCSV
+    )
+where
+
+import           Text.ParserCombinators.Parsec
+
+-- Begin copypasta --
+-- http://book.realworldhaskell.org/read/using-parsec.html
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV = parse csvFile "(stdin)"
+
+csvFile :: GenParser Char st [[String]]
+csvFile = endBy line eol
+
+line :: GenParser Char st [String]
+line = sepBy cell (char ',')
+
+cell :: GenParser Char st String
+cell = quotedCell <|> many (noneOf ",\n\r")
+
+quotedCell :: GenParser Char st String
+quotedCell = do
+    char '"'
+    content <- many quotedChar
+    char '"' <?> "quote at end of cell"
+    return content
+
+quotedChar :: GenParser Char st Char
+quotedChar = noneOf "\"" <|> try (string "\"\"" >> return '"')
+
+eol :: GenParser Char st String
+eol =
+    try (string "\n\r")
+        <|> try (string "\r\n")
+        <|> string "\n"
+        <|> string "\r"
+        <?> "end of line"
--- src/Table.hs
+++ src/Table.hs
@@ -9,7 +9,7 @@ where
 
 import           Data.List
 import           Data.List.Split                ( splitOneOf )
-import           Text.ParserCombinators.Parsec
+import qualified Parser
 
 type Header = String
 type Row = [String]
@@ -21,55 +21,16 @@ type Table = [Row]
 -- , ["CREDIT",           "6/3/20", "Checking",     "PAYCHECK",    "500",    "",              "$500",    ""]
 -- ]
 toTable :: String -> Table
-toTable contents = case parse csvFile "(stdin)" contents of
-    Left  e -> [["Error parsing input:" ++ (show e)]]
-    Right r -> r
+toTable contents = case Parser.parseCSV contents of
+    Left  e     -> [["Error parsing input:" ++ (show e)]]
+    Right table -> table
 
 toCSV :: Table -> String
 toCSV = genCsvFile
 
--- This implementation only works when there are no commas within the table cells. When there are, 
--- those substrings get split up, resulting in too many cells for that row. Copied the example used in the Parsec
--- library to fix the bugs I was encountering, although I would've liked to write my own solution.
-toRowsAndColumns :: String -> Table
-toRowsAndColumns csv = map (splitOneOf ",") (lines csv)
-
 getCell :: Table -> Header -> Row -> Maybe String
 getCell (headers : rows) header row =
     (header `elemIndex` headers) >>= \i -> return (row !! i)
-
--- Begin copypasta --
--- http://book.realworldhaskell.org/read/using-parsec.html
-
-csvFile :: GenParser Char st [[String]]
-csvFile = endBy line eol
-
-line :: GenParser Char st [String]
-line = sepBy cell (char ',')
-
-cell :: GenParser Char st String
-cell = quotedCell <|> many (noneOf ",\n\r")
-
-quotedCell :: GenParser Char st String
-quotedCell = do
-    char '"'
-    content <- many quotedChar
-    char '"' <?> "quote at end of cell"
-    return content
-
-quotedChar :: GenParser Char st Char
-quotedChar = noneOf "\"" <|> try (string "\"\"" >> return '"')
-
-eol :: GenParser Char st String
-eol =
-    try (string "\n\r")
-        <|> try (string "\r\n")
-        <|> string "\n"
-        <|> string "\r"
-        <?> "end of line"
-
-parseCSV :: String -> Either ParseError [[String]]
-parseCSV = parse csvFile "(unknown)"
 
 -- Other copypasta from Data.CSV -- 
 -- https://hackage.haskell.org/package/MissingH-1.4.3.0/docs/src/Data.CSV.html#genCsvFile
