444a076313ca3a95245fb988cd462d7a597ce84c
Nick Rotondo <nickrotondo724@gmail.com>
2020-07-08 15:24:34 -0400

    rename

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,7 @@ main = do
 getCashDiff :: IO Float
 getCashDiff = do
     putStrLn "How much cash on-hand now?"
-    remainingCash <- getLine >>= parseCashInputOr getCashDiff
+    remainingCash <- getLine >>= readCashInputOr getCashDiff
     startingCash  <- getStartingCash
     return (remainingCash - startingCash)
   where
@@ -24,8 +24,8 @@ getCashDiff = do
     getStartingCash = do
         putStrLn "No info found for cash on-hand to start the month..." -- TODO 
         putStrLn "How much did you have on-hand at the start of this month?"
-        getLine >>= parseCashInputOr getStartingCash
-    parseCashInputOr :: IO Float -> String -> IO Float
-    parseCashInputOr onFail cash = case readMaybe cash of
+        getLine >>= readCashInputOr getStartingCash
+    readCashInputOr :: IO Float -> String -> IO Float
+    readCashInputOr onFail cash = case readMaybe cash of
         Nothing  -> putStrLn "Needs to be a number. Try Again." >> onFail
         Just amt -> return amt
