a96b786dc8e3c677085ef931e4ceef4c566411c2
Nick Rotondo <nickrotondo724@gmail.com>
2020-08-01 11:00:32 -0400

    Generalize language, remove print statement

--- app/Main.hs
+++ app/Main.hs
@@ -11,19 +11,17 @@ main = do
     let wPath = "../../Downloads/monthly-budget.csv"
     writeFile wPath $ monthlyBudget cashDiff contents
     putStrLn $ "Your monthly budget can be found at: " ++ wPath
-    print cashDiff -- Just for dev purposes
 
 getCashDiff :: IO Float
 getCashDiff = do
-    putStrLn "How much cash on-hand now?"
+    putStrLn "How much cash on-hand at end of period?"
     remainingCash <- getLine >>= readCashInputOr getCashDiff
     startingCash  <- getStartingCash
     return (remainingCash - startingCash)
   where
     getStartingCash :: IO Float
     getStartingCash = do
-        putStrLn "No info found for cash on-hand to start the month..." -- TODO 
-        putStrLn "How much did you have on-hand at the start of this month?"
+        putStrLn "How much did you have on-hand at the start of this period?"
         getLine >>= readCashInputOr getStartingCash
     readCashInputOr :: IO Float -> String -> IO Float
     readCashInputOr onFail cash = case readMaybe cash of
