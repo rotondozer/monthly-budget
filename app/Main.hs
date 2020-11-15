9b9e131c91e34ea2b4cfc909e173abc6dc5d5829
Nick Rotondo <nickrotondo724@gmail.com>
2020-11-15 15:31:05 -0500

    Readme update plus formatter defaults changed?

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,43 @@
 module Main where
 
-import           Lib                            ( monthlyBudget )
-import           Text.Read                      ( readMaybe )
+import Debug.Trace (trace)
+import Lib (monthlyBudget)
 import qualified Parser
 import qualified Table
-import           Debug.Trace                    ( trace )
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
-    table    <- csvToTable
-    cashDiff <- getCashDiff
-    let wPath = "../../Downloads/monthly-budget.csv"
-    writeFile wPath $ monthlyBudget cashDiff table
-    putStrLn $ "Your monthly budget can be found at: " ++ wPath
+  table <- csvToTable
+  cashDiff <- getCashDiff
+  let wPath = "../../Downloads/monthly-budget.csv"
+  writeFile wPath $ monthlyBudget cashDiff table
+  putStrLn $ "Your monthly budget can be found at: " ++ wPath
 
 csvToTable :: IO Table.Table
 csvToTable =
-    putStrLn "Path to CSV (relative to this program):"
-        >>  getLine
-        >>= readFile
-        >>= toTable
+  putStrLn "Path to CSV (relative to this program):"
+    >> getLine
+    >>= readFile
+    >>= toTable
   where
     toTable :: String -> IO Table.Table
     toTable c = case Parser.parseCSV c of
-        Left e -> trace (show e) putStrLn "Failed reading CSV!" >> csvToTable
-        Right table -> return table
-
+      Left e -> trace (show e) putStrLn "Failed reading CSV!" >> csvToTable
+      Right table -> return table
 
 getCashDiff :: IO Float
 getCashDiff = do
-    putStrLn "How much cash on-hand at end of period?"
-    remainingCash <- getLine >>= readCashInputOr getCashDiff
-    startingCash  <- getStartingCash
-    return (remainingCash - startingCash)
+  putStrLn "How much cash on-hand at end of period?"
+  remainingCash <- getLine >>= readCashInputOr getCashDiff
+  startingCash <- getStartingCash
+  return (remainingCash - startingCash)
   where
     getStartingCash :: IO Float
     getStartingCash = do
-        putStrLn "How much did you have on-hand at the start of this period?"
-        getLine >>= readCashInputOr getStartingCash
+      putStrLn "How much did you have on-hand at the start of this period?"
+      getLine >>= readCashInputOr getStartingCash
     readCashInputOr :: IO Float -> String -> IO Float
     readCashInputOr onFail cash = case readMaybe cash of
-        Nothing  -> putStrLn "Needs to be a number. Try Again." >> onFail
-        Just amt -> return amt
+      Nothing -> putStrLn "Needs to be a number. Try Again." >> onFail
+      Just amt -> return amt
--- src/CashFlow.hs
+++ src/CashFlow.hs
@@ -1,21 +1,23 @@
 module CashFlow
-    ( Amount
-    , Description
-    , CashFlow
-    , addToDebsAndCreds
-    , toMatrix
-    , readAmount
-    , toUniqueListWithAmountSum
-    )
+  ( Amount,
+    Description,
+    CashFlow,
+    addToDebsAndCreds,
+    toMatrix,
+    readAmount,
+    toUniqueListWithAmountSum,
+  )
 where
 
-import           Data.List
-import           Data.Maybe                     ( fromMaybe )
-import           Text.Read                      ( readMaybe )
-import           Text.Printf
+import Data.List
+import Data.Maybe (fromMaybe)
+import Text.Printf
+import Text.Read (readMaybe)
 
 type Amount = Float
+
 type Description = String
+
 type CashFlow = (Description, Amount)
 
 readAmount :: String -> Float
@@ -23,7 +25,7 @@ readAmount a = fromMaybe 0 (readMaybe a)
 
 addToDebsAndCreds :: CashFlow -> [[CashFlow]] -> [[CashFlow]]
 addToDebsAndCreds cf@(_, amt) (debits : credits : _) =
-    if amt < 0 then [debits ++ [cf], credits] else [debits, credits ++ [cf]]
+  if amt < 0 then [debits ++ [cf], credits] else [debits, credits ++ [cf]]
 
 total :: [CashFlow] -> Amount
 total = foldl (\tot (_, amt) -> tot + amt) 0
@@ -36,24 +38,24 @@ toUniqueListWithAmountSum = foldl addAmounts []
   where
     addAmounts :: [CashFlow] -> CashFlow -> [CashFlow]
     addAmounts uniqList cf@(desc, amount) = case (lookup desc uniqList) of
-        Nothing  -> uniqList ++ [cf]
-        Just amt -> (deleteBy isSameDesc cf uniqList) ++ [(desc, amt + amount)]
+      Nothing -> uniqList ++ [cf]
+      Just amt -> (deleteBy isSameDesc cf uniqList) ++ [(desc, amt + amount)]
     isSameDesc :: (CashFlow -> CashFlow -> Bool)
     isSameDesc (desc1, _) (desc2, _) = desc1 == desc2
 
 toMatrix :: [[CashFlow]] -> [[String]]
 toMatrix (debits : credits : _) =
-    let totalCred = total credits
-        totalDeb  = total debits
-    in  (["--- DEBITS ---"] : (toSortedList debits))
-            ++ (sectionSeparator : ["--- CREDITS ---"] : (toSortedList credits))
-            ++ [ sectionSeparator
-               , ["Total Credits", round2Dec totalCred]
-               , ["Total Debits", round2Dec totalDeb]
-               , ["NET", round2Dec $ totalCred + totalDeb]
-               ]
+  let totalCred = total credits
+      totalDeb = total debits
+   in (["--- DEBITS ---"] : (toSortedList debits))
+        ++ (sectionSeparator : ["--- CREDITS ---"] : (toSortedList credits))
+        ++ [ sectionSeparator,
+             ["Total Credits", round2Dec totalCred],
+             ["Total Debits", round2Dec totalDeb],
+             ["NET", round2Dec $ totalCred + totalDeb]
+           ]
   where
-    toSortedList     = (map toList) . sortCashFlows
+    toSortedList = (map toList) . sortCashFlows
     sectionSeparator = ["", "----------"]
 
 -- Sort the greater absolute value to be higher, so highest expense and highest credit appear first
